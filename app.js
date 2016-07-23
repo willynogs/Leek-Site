68f7760395175958a80ecba56836cea76b599829
willynogs <willynogs@gmail.com>
2016-07-22 23:13:06 -0400

    svg lander

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
 $(function(){
   /*
-  var about = new OriDomi('.about');
-  var events = new OriDomi('.events');
-  var media = new OriDomi('.media');
-  var booking = new OriDomi('.booking');
-  */
-  rectDimensions();
-  /*
-  $('.fa-twitter').click(function(){
-    $('.content').addClass('content-animate');
-  });
-  */
-  /*
-  $('.content').hover(function(){
-    $(this).animate({
-      "background-size": "110%"
-    }, 2000, "linear");
-  }, function(){
-    $(this).animate({
-      "background-size": "100%"
-    }, 2000, "linear");
-  });
+  setTimeout(function(){
+    $('span').text('dj leek');
+  }, 3000);
   */
 });
-
-function rectDimensions () {
-  var vpw = $(window).width();
-  var vph = $(window).height();
-  var actualVph = (vph - 90) / 2;
-  $('.content').css({
-    "height": actualVph + "px"
-  });
-}
