c264d9d86f082abc80e6796ab8e3ce6f49ccd29c
willynogs <willynogs@gmail.com>
2016-07-26 00:31:40 -0400

    starting with rendering panes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(function(){
-  
+  $('.about').click(function(){
+    if($('#about').hasClass('before-animate')){
+      $('#about').removeClass('before-animate');
+      $('#about').addClass('animate-frame');
+    } else {
+      $('#about').removeClass('animate-frame');
+      $('#about').addClass('before-animate');
+    }
+  });
 });
