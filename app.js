d3d6d8f3773a378464e2007a79fba87c612965fd
willynogs <willynogs@gmail.com>
2016-07-28 22:37:49 -0400

    Add social media icons

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 $(function(){
-  $('.about').click(function(){
-    if($('#about').hasClass('before-animate')){
-      $('#about').removeClass('before-animate');
-      $('#about').addClass('animate-frame');
-    } else {
-      $('#about').removeClass('animate-frame');
-      $('#about').addClass('before-animate');
-    }
-  });
+  $('.credit').hide();
 });
